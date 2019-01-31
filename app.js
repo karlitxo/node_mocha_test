50a805ed3d12561acc518537d68390afe4087a50
karlitxo <karlitxo@gmail.com>
2019-01-31 20:08:51 +0000

    typos, npm start to run w/ pm2

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express()
 app.set('port', process.env.PORT || 3000)
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
-app.set('app-name', 'node test')
+app.set('app-name', 'node ci test')
 
 app.all('*', (req, res) => {
   res.render('index', {title: 'Howdy, stranger!', msg: 'Welcome to ' + app.get('app-name')})
